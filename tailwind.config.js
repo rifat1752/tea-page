1aa719fa6486e0316f3d6097c8938e3067be7ca0
rifat1752 <rifathossain1752@gmail.com>
2023-07-24 23:21:01 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            tea:{
+                btn:"linear-gradient(270deg, #F00 0%, #FF8938 100%))"
+            }
+        }
+      extend: {},
+    },
+    plugins: [],
+  }
